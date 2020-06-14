780ff5d432566175de0fbb213c9e2cdf5eae5192
Dabeer <DabeerM@hotmail.com>
2020-06-14 21:40:26 +0100

    We can now call the API for multiples places given the latitude and longitude

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask
 from flask import jsonify
+from flask import request # Allows us to take multiple arguments
 import requests # To get users from API provided
 import json
 from math import radians, cos, sin, asin, sqrt
@@ -9,7 +10,8 @@ from typing import List
 
 app = Flask(__name__)
 
-# Helper functions
+# -------------------------   Helper functions -------------------------
+
 def getUsers() -> List[dict]:
 	'''
 	This function returns all the users from the API given
@@ -35,17 +37,30 @@ def haversine(lat1:float, lon1:float, lat2:float, lon2:float) -> float:
 
 	return R * c
 
+# -------------------------- Main code ---------------------------------
+
+# You can call this function using the get method at http://localhost:5000/users/London
+# Structured this way as you may later want to extend the API to include other functionality
 @app.route('/users/London', methods=['GET'])
-def getAllUsersUnderFiftyMiles():
-	# This gets us the users
+def getAllUsersUnderFiftyMiles() -> List[dict]:
+	''' This allows us to alter where we're searching from generalising the API
+		This is advantageous as some people may use slightly different coordinates for London
+		and this gives them that flexibility. Will default to (51.507222, -0.1275) for 
+		the latitude and longitude respectively if no values are provided in the request
+	'''
+	london_lat = request.args.get('latitude', 51.507222)
+	london_long =  request.args.get('longitude', -0.1275)
+
+	# This gets us the users from https://bpdts-test-app.herokuapp.com
 	all_users = getUsers()
 
 	# Now we need to filter out the ones not within 50 miles
-	london_lat, london_long = 51.507222, -0.1275
-	users_fifty_miles = list(filter(lambda person: haversine(london_lat, london_long, float(person['latitude']), float(person['longitude'])) <= 50.0, all_users))
+	max_distance = 50.0 # The maximum distance from London, 50 miles
+	users_fifty_miles = list(filter(lambda person: haversine(london_lat, london_long, float(person['latitude']), float(person['longitude'])) <= max_distance, all_users))
 
 	# Returning a json response
 	return jsonify(users_fifty_miles)
 
+# Starting the application/service
 if __name__ == '__main__':
 	app.run(debug=False)
